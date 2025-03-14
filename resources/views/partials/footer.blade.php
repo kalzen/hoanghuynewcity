ac453518801c968dec6401b8116e66b639329e16
kalzen <manhto109@gmail.com>
2025-03-14 11:28:05 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,9 +6,9 @@
 					<div class="col-md-4 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.3s">
                         <div class="widget widget_about">
 							<div class="footer-logo">
-								<a href="index.html"><img src="images/logo-white.png" alt=""/></a> 
+								<a href="index.html"><img src="{{asset('images/logo/logo.png')}}" alt=""/></a> 
 							</div>
-							<p>Surround yourself with fresh energy, high-tech amenities, and elevated style. Indulge in extraordinary amenities, relax in appealing social spaces.</p>
+							<p>Khu đô thị mới Hoàng Huy New City là dự án Nhà liền kề, shophouse và chung cư với tổng diện tích đất lớn 65ha do Tập đoàn tài chính Hoàng Huy làm chủ đầu tư tọa lạc tại thành phố Thủy Nguyên, Hải Phòng.</p>
 							<div class="dlab-social-icon">
 								<ul>
 									<li><a class="fa fa-facebook" href="javascript:void(0);"></a></li>
@@ -21,23 +21,12 @@
                     </div>
 					<div class="col-md-4 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.6s">
 						<div class="widget">
-							<h5 class="footer-title">Contact Us</h5>
+							<h5 class="footer-title">Thông tin liên hệ</h5>
 							<ul class="contact-info-bx">
 								<li><i class="las la-map-marker"></i><strong>Address</strong> Xã Tân Dương, huyện Thủy Nguyên</li>
 								<li><i class="las la-phone-volume"></i><strong>Call :-</strong> 0928291291</li>
 								<li><i class="las la-phone-square-alt"></i><strong>Email :-</strong> phamvantu29121991@gmail.com</li>
 							</ul>
-						</div>
-                    </div>
-					<div class="col-md-4 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.9s">
-						<div class="widget widget-logo">
-						   <h5 class="footer-title">Our Business Channels</h5>
-						   <ul>
-								<li><a href="javascript:void(0);"><img src="images/logo/logo1.png" alt=""/></a></li>
-								<li><a href="javascript:void(0);"><img src="images/logo/logo2.png" alt=""/></a></li>
-								<li><a href="javascript:void(0);"><img src="images/logo/logo3.png" alt=""/></a></li>
-								<li><a href="javascript:void(0);"><img src="images/logo/logo4.png" alt=""/></a></li>
-						   </ul>
 						</div>
                     </div>
                 </div>
@@ -51,9 +40,9 @@
                     <div class="col-md-6 col-sm-12 text-md-right text-center"> 
 						<div class="widget-link "> 
 							<ul>
-								<li><a href="about-us-1.html"> About</a></li>
-								<li><a href="contact-us-1.html"> Contact Us</a></li>
-								<li><a href="privacy-policy.html"> Privacy Policy</a></li>
+								<li><a href="#"> Về chúng tôi</a></li>
+								<li><a href="#"> Liên hệ với chúng tôi</a></li>
+								<li><a href="#"> Privacy Policy</a></li>
 							</ul>
 						</div>
 					</div>
